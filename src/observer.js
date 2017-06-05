377dff507ed91d2e09cbb8c35d8f4ce8ab57005b
Andrea Di Saverio <disaverio.andrea@gmail.com>
2017-06-06 00:01:20 +0200

    tuning

--- src/observer.js
+++ src/observer.js
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2016 Andrea Di Saverio
+ * Copyright (c) 2017 Andrea Di Saverio
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
